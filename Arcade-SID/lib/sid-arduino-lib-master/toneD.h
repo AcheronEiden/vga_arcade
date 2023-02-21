9db9216f4c45bb83c5e53c1a47bbe394cfcdc6ac
mkamalmaz <m.kamalmaz@gmail.com>
2023-02-21 22:14:44 +0100

    20230221

---
+++ Arcade-SID/lib/sid-arduino-lib-master/toneD.h
@@ -0,0 +1,2 @@
+const int sidLength4 = 1;  //number of bytes in array
+const char sidData4[] PROGMEM = {1};
--- Arcade-SID/src/main.cpp
+++ Arcade-SID/src/main.cpp
@@ -1,7 +1,7 @@
 /*
- VGA Arcade SID - Version 20230210
+ VGA Arcade SID - Version 20230221
 
- CHANGES 2022-12-21 -- 2023-02-10 FOR 1TE663 PROJECT.
+ CHANGES 2022-12-21 -- 2023-02-21 FOR 1TE663 PROJECT.
  CHANGES BY TOBIAS HOLM (/TH:) AND MOHAMMED NOUR KAMALMAZ (/MK:)
 */
 
@@ -12,7 +12,10 @@
 #include <inttypes.h>
 #include <String.h>
 #include <avr/pgmspace.h>  //used to store data in the flash rom
-#include "tone3.h" //RAW SID register data file in flash
+#include "toneA.h" //RAW SID register data file in flash, Gameplay music (play at start)
+#include "toneB.h" //RAW SID register data file in flash, Ball hit pad
+#include "toneC.h" //RAW SID register data file in flash, Ball hit block
+#include "toneD.h" //RAW SID register data file in flash, Game Over
 
 /************************************************************************
 
@@ -74,41 +77,113 @@
  http://www.ffd2.com/fridge/chacking/
  Specifically the Rob Hubbard play subroutine as that is the code
  all serious C64 SID musicians must understand.
- 
  convert the dump to number with : http://www.expertsetup.com/~brian/c64/JConverter.jar / http://softcollection.sytes.net/javaprog
  
 */
-
+// https://github.com/daitangio/sid-arduino-lib/blob/master/examples/SID_I2C_receiver/SID_I2C_receiver.ino
 // Connect PIN 9 from your arduino Uno to audio output (+ ground to the other pin of the audio output)!
 
 
-// https://github.com/daitangio/sid-arduino-lib/blob/master/examples/SID_I2C_receiver/SID_I2C_receiver.ino
+
+/*
+//MK
+// code for master arduino if we use Wire.h
+Wire.beginTransmission(23);   // arbitrary address 23                                                
+Wire.write('A');  // or 'B', 'C', 'D' (which sound effect we want to play)                                          
+Wire.endTransmission();
+-----------------------------------------------------------------------------------------------------------------
+If the arduino is overwhelmed, we can make a separate function called for instance receiveEvent, and add Wire.onReceive(receiveEvent) to the setup. This will direct the program to
+the specified function in case a message is recieved. The main loop can then include a delay for example. 
+
+*/
 
 SID mySid;
 
 
 void setup()  {
-  Wire.begin(23);                // join i2c bus with address #23
-  Wire.onReceive(receiveEvent); // register event  
+  Wire.begin(23); // join i2c bus with address #23
   mySid.begin();
-      } 
+  
+} 
 
-void loop() {
-  for(uint16_t sidPointer=0;sidPointer<=sidLength;sidPointer++){
+void functionA() {
+for(uint16_t sidPointer=0;sidPointer<=sidLength1;sidPointer++){
     for(uint8_t sidRegister=0;sidRegister<=24;sidRegister++){
-      mySid.set_register(sidRegister, (pgm_read_byte(&sidData[(sidPointer+sidRegister)])));
+      mySid.set_register(sidRegister, (pgm_read_byte(&sidData1[(sidPointer+sidRegister)])));
     };
     delay(19);
     sidPointer=sidPointer+24;
   };
+  // set all registers to 0 to stop playing music/ stop noise from continuing
+  delay(19);
+  for(uint8_t sidRegister=0;sidRegister<=24;sidRegister++){
+      mySid.set_register(sidRegister, 0);
+  };
 }
 
-void receiveEvent(int howMany)
-{
-  while ( Wire.available()) // loop through all 
-  {
-    uint8_t reg = Wire.read(); 
-    uint8_t val = Wire.read();    
-    mySid.set_register(reg,val);        
+void functionB() {
+for(uint16_t sidPointer=0;sidPointer<=sidLength2;sidPointer++){
+    for(uint8_t sidRegister=0;sidRegister<=24;sidRegister++){
+      mySid.set_register(sidRegister, (pgm_read_byte(&sidData2[(sidPointer+sidRegister)])));
+    };
+    delay(19);
+    sidPointer=sidPointer+24;
+  };
+  // set all registers to 0 to stop playing music/ stop noise from continuing
+  delay(19);
+  for(uint8_t sidRegister=0;sidRegister<=24;sidRegister++){
+      mySid.set_register(sidRegister, 0);
+  };
+}
+
+void functionC() {
+for(uint16_t sidPointer=0;sidPointer<=sidLength3;sidPointer++){
+    for(uint8_t sidRegister=0;sidRegister<=24;sidRegister++){
+      mySid.set_register(sidRegister, (pgm_read_byte(&sidData3[(sidPointer+sidRegister)])));
+    };
+    delay(19);
+    sidPointer=sidPointer+24;
+  };
+  // set all registers to 0 to stop playing music/ stop noise from continuing
+  delay(19);
+  for(uint8_t sidRegister=0;sidRegister<=24;sidRegister++){
+      mySid.set_register(sidRegister, 0);
+  };
+}
+
+void functionD() {
+for(uint16_t sidPointer=0;sidPointer<=sidLength4;sidPointer++){
+    for(uint8_t sidRegister=0;sidRegister<=24;sidRegister++){
+      mySid.set_register(sidRegister, (pgm_read_byte(&sidData4[(sidPointer+sidRegister)])));
+    };
+    delay(19);
+    sidPointer=sidPointer+24;
+  };
+  // set all registers to 0 to stop playing music/ stop noise from continuing
+  delay(19);
+  for(uint8_t sidRegister=0;sidRegister<=24;sidRegister++){
+      mySid.set_register(sidRegister, 0);
+  };
+}
+
+void loop() {
+  while (Wire.available() > 0) {
+  char command = Wire.read();
+    if (command == 'A') 
+    {
+      functionA();
+    }
+    else if (command == 'B')
+    {
+      functionB();
+    }
+    else if (command == 'C')
+    {
+      functionC();
+    }
+    else if (command == 'C')
+    {
+      functionC();
+    }
   }
 }
